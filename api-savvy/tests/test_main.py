51e0dbe2cf1246ec0f2098432965282b2845ee74
Asier Aduriz <asier.aduriz@simlocal.com>
2025-03-15 16:24:08 +0100

    Create conftest

--- api-savvy/tests/test_main.py
+++ api-savvy/tests/test_main.py
@@ -1,31 +1,7 @@
-import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
+from sqlmodel import Session
 
-from api.main import Hero, app, get_session
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-
-
-@pytest.fixture(name="client")
-def client_fixture(session: Session):
-    def get_session_override():
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
-
+from api.main import Hero
 
 def test_create_hero(client: TestClient):
     response = client.post(
