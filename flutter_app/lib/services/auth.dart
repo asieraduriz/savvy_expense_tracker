1e5e04408e723f37665dd8f8fca31ae2c192700b
Asier Aduriz <asier.aduriz@simlocal.com>
2025-03-29 16:22:03 +0100

    Add simple localhost toggle between android and ios

--- flutter_app/lib/services/auth.dart
+++ flutter_app/lib/services/auth.dart
@@ -1,15 +1,23 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 class AuthService {
+  late String baseUrl;
+
+  AuthService() {
+    baseUrl =
+        Platform.isAndroid ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
+  }
+
   Future<http.Response> signup(
     String name,
     String email,
     String password,
   ) async {
     return http.post(
-      Uri.parse('http://localhost:8000/auth/signup/'),
+      Uri.parse('$baseUrl/auth/signup/'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -23,7 +31,7 @@ class AuthService {
 
   Future<http.Response> login(String email, String password) async {
     return http.post(
-      Uri.parse('http://localhost:8000/auth/login/'),
+      Uri.parse('$baseUrl/auth/login/'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
